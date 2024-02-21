e7e1663633fed555bc51ca078c8fcf311c931a66
Robin Shah <robin123shah@gmail.com>
2024-02-21 10:43:29 +0530

    BACKEND

--- src/components/Navigation/Router.js
+++ src/components/Navigation/Router.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Table from "../table";
 import NavBar from "./NavBar";
-import Addmatch from "../AddMatch";
+import Addmatch from "../Addmatch";
 import Register from "../Register";
 const Router = () => {
     return (
